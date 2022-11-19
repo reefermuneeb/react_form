474945be1d44d3c0e3c57f930b48545ad4d2d929
reefermuneeb <112246341+reefermuneeb@users.noreply.github.com>
2022-11-19 14:51:12 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Table from './coomponents/Table'
+
+
+function App() {
+  return (
+    <div className="bg-gray-100 h-screen">
+      <div className='flex justify-center justify-items-center' >
+      <Table />
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
